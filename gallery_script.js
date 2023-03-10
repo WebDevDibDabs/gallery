4b5dcd3085be214ad301b10a3b8062f43b20105c
WebDevDibDabs <126905587+WebDevDibDabs@users.noreply.github.com>
2023-03-10 08:26:42 +0000

    Update gallery_script.js

--- gallery_script.js
+++ gallery_script.js
@@ -1,12 +1,5 @@
 const firebaseConfig = {
-    /*apiKey: "AIzaSyBYwa7ifHMP_z1MsZ7CBRTGY_aqnSp4XeE",
-    authDomain: "image-upload-39545.firebaseapp.com",
-    projectId: "image-upload-39545",
-    storageBucket: "image-upload-39545.appspot.com",
-    messagingSenderId: "284565950625",
-    appId: "1:284565950625:web:975404091286cbebcf60e0"*/
-	
-	apiKey: "AIzaSyChettIA_eN1Z7LRDAbGxcvWD4kpJhhBMI",
+    	apiKey: "AIzaSyChettIA_eN1Z7LRDAbGxcvWD4kpJhhBMI",
 	authDomain: "imageuploads-ec685.firebaseapp.com",
 	projectId: "imageuploads-ec685",
 	storageBucket: "imageuploads-ec685.appspot.com",
